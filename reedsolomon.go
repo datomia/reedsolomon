f6cb7a2fb84a14736d0453fe5817500ddf6b7b5f
dssysolyatin <sysolyatin.dima@yandex.ru>
2019-06-20 18:31:56 +0300

    Improve memory usage

--- reedsolomon.go
+++ reedsolomon.go
@@ -863,9 +863,7 @@ func (r reedSolomon) Split(data []byte) ([][]byte, error) {
 	// Only allocate memory if necessary
 	if len(data) < (r.Shards * perShard) {
 		// Pad data to r.Shards*perShard.
-		tmpBuf := make([]byte, r.Shards*perShard)
-		copy(tmpBuf, data)
-		data = tmpBuf
+		data = append(data, make([]byte, r.Shards*perShard-len(data))...)
 	}
 
 	// Split into equal-length shards.
